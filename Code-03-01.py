d5357daeed3737f374ee19053d76ba0118c1a9b9
yungun12 <162670150+yungun12@users.noreply.github.com>
2024-06-27 15:01:11 +0900

    선형 리스트(선형 리스트 생성 함수 코드)

---
+++ Code-03-01.py
@@ -0,0 +1,13 @@
+kapo = []
+
+def add_data(friend):
+    katok.append(None)
+    kLen = len(katok)
+    katok[kLen-1] = friend
+
+while true:
+    data = input("추가할 데이터 -->")
+    add_data(data)
+
+print(katok)
+
